11e136ae4fd12c2642aae0218577bbef7a23007f
Gideon <keterjr1@gmail.com>
2024-06-12 13:57:04 +0300

    pass tests

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,17 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        def dfs(node):
+            if node is None:
+                return None
+            if node.get('id') == id:
+                return node
+            for child in node.get('children', []):
+                result = dfs(child)
+                if result is not None:
+                    return result
+            return None
+
+        return dfs(self.root)
